fbd0b947bf1404eb0e38db52b793e04e40d58e36
Chugunov Vladimir <inflamepheonix@gmail.com>
2013-10-02 14:42:42 +0400

    Tile refactoring

--- src/com/avapir/roguelike/Roguelike.java
+++ src/com/avapir/roguelike/Roguelike.java
@@ -8,23 +8,14 @@ import com.avapir.roguelike.core.Game;
 
 public class Roguelike {
 
-	public static final List<Integer> al = new ArrayList<Integer>(){
-		{
-			add(1);
-			add(2);
-			add(3);
-		}
-	};
-	
 	public static void main(String[] args) {
 //		 Game game = new Game();
 //		 game.init();
 //		 game.run();
 //		 game.done();
-		List<Integer> l  = al;
-		al.add(1);
-		l.add(2);
-		System.out.println(al);
-		System.out.println(l);
+		System.out.println(Integer.toString(1<<0, 2));
+		System.out.println(Integer.toString(1<<1, 2));
+		System.out.println(Integer.toString(1<<2, 2));
+		System.out.println(Integer.toString(1<<3, 2));
 	}
 }
--- src/com/avapir/roguelike/Tile.java
+++ src/com/avapir/roguelike/Tile.java
@@ -50,42 +50,43 @@ public class Tile {
 		public static final int FULL_FLAG 		= 0b11111111111111111111111111111111;
 		public static final int EMPTY_FLAG		= 0b00000000000000000000000000000000;
 		
-		/*
-		 * 1-9 FOV
-		 * 10-15 terrain and moving
-		 */
-		public static final int F1 				= 0b00000000000000000000000000000001;//VISIBLE
-		public static final int F2 				= 0b00000000000000000000000000000010;//SEEN
-		public static final int F3 				= 0b00000000000000000000000000000100;//LIGHT_ON
-		public static final int F4				= 0b00000000000000000000000000001000;//TRANSPARENT
-		public static final int F5 				= 0b00000000000000000000000000010000;
-		public static final int F6 				= 0b00000000000000000000000000100000;
-		public static final int F7 				= 0b00000000000000000000000001000000;
-		public static final int F8 				= 0b00000000000000000000000010000000;
-		public static final int F9 				= 0b00000000000000000000000100000000;
-		public static final int F10				= 0b00000000000000000000001000000000;//PASSABLE
-		public static final int F11				= 0b00000000000000000000010000000000;//EMPTY -- ничего
-		public static final int F12				= 0b00000000000000000000100000000000;//GRASS -- may heal/gain luck
-		public static final int F13				= 0b00000000000000000001000000000000;//STONES -- may fall(dont go and lose hp)
-		public static final int F14				= 0b00000000000000000010000000000000;//ICE -- may slip(lose hp, gain luck)
-		public static final int F15				= 0b00000000000000000100000000000000;//POISIONING -- DoT, decreases dmg from lightning
-		public static final int F16				= 0b00000000000000001000000000000000;//FLAMING -- DoT, incr
-		public static final int F17				= 0b00000000000000010000000000000000;//WET -- removes flame, decreases dmg from lightning
-		public static final int F18				= 0b00000000000000100000000000000000;
-		public static final int F19				= 0b00000000000001000000000000000000;
-		public static final int F20				= 0b00000000000010000000000000000000;
-		public static final int F21				= 0b00000000000100000000000000000000;
-		public static final int F22				= 0b00000000001000000000000000000000;
-		public static final int F23				= 0b00000000010000000000000000000000;
-		public static final int F24				= 0b00000000100000000000000000000000;
-		public static final int F25				= 0b00000001000000000000000000000000;
-		public static final int F26				= 0b00000010000000000000000000000000;
-		public static final int F27				= 0b00000100000000000000000000000000;
-		public static final int F28				= 0b00001000000000000000000000000000;
-		public static final int F29				= 0b00010000000000000000000000000000;
-		public static final int F30				= 0b00100000000000000000000000000000;
-		public static final int F31				= 0b01000000000000000000000000000000;//UP_LADDER
-		public static final int F32				= 0b10000000000000000000000000000000;//DOWN_LADDER
+												/*0-8 FOV*/
+		public static final int F0 = 1<<0;//VISIBLE
+		public static final int F1 = 1<<1;//SEEN
+		public static final int F2 = 1<<2;//LIGHT_ON
+		public static final int F3 = 1<<3;//TRANSPARENT
+		public static final int F4 = 1<<4;
+		public static final int F5 = 1<<5;
+		public static final int F6 = 1<<6;
+		public static final int F7 = 1<<7;
+		public static final int F8 = 1<<8;
+									/* 9-14 terrain and moving */
+		public static final int F9 = 1<<9;//PASSABLE
+		public static final int F10 = 1<<10;//EMPTY -- nothing
+		public static final int F11	= 1<<11;//GRASS -- may heal/gain luck
+		public static final int F12	= 1<<12;//STONES -- may fall(dont go and lose hp)
+		public static final int F13	= 1<<13;//ICE -- may slip(lose hp, gain luck)
+									/* 14-24 gaining effects */
+		public static final int F14	= 1<<14;//POISIONING -- DoT, decreases dmg from lightning
+		public static final int F15	= 1<<15;//FLAMING -- DoT, dramatically increases dmg from lightning
+		public static final int F16	= 1<<16;//WET -- removes flame, increases dmg from lightning
+		public static final int F17	= 1<<17;//INSTANT_DEATH -- you can avoid it only using smth expensive BEFORE IT (consumable, rare item, etc.)
+		public static final int F18	= 1<<18;
+		public static final int F19	= 1<<19;
+		public static final int F20	= 1<<20;
+		public static final int F21	= 1<<21;
+		public static final int F22	= 1<<22;
+		public static final int F23	= 1<<23;
+									/* 24-31 specials */		
+		public static final int F24	= 1<<24;
+		public static final int F25	= 1<<25;
+		public static final int F26	= 1<<26;
+		public static final int F27	= 1<<27;
+		public static final int F28	= 1<<28;//OPEN_DOOR
+		public static final int F29	= 1<<29;//CLOSED_DOOR
+		public static final int F30	= 1<<30;//UP_LADDER
+		public static final int F31	= 1<<31;//DOWN_LADDER
+		
 	}
 	
 	private boolean checkFlag(int flag) {
@@ -108,54 +109,29 @@ public class Tile {
 		}
 	}
 	
-	public boolean isEmpty(){
-		return checkFlag(Flag.F11);
-	}
+	public boolean isVisible() 		{return checkFlag(Flag.F0);}
+	public boolean isSeen() 		{return checkFlag(Flag.F1);}
+	public boolean isLantern() 		{return checkFlag(Flag.F2);}
+	public boolean isTransparent() 	{return checkFlag(Flag.F3);}
 	
-	public boolean isPassable() { 
-		return checkFlag(Flag.F10);
-	}
+	public boolean isPassable() 	{return checkFlag(Flag.F9);}
+	public boolean isEmpty()		{return checkFlag(Flag.F10);}
 	
-	public boolean isUpLadder() {
-		return checkFlag(Flag.F31);
-	}
+	public boolean isGrass() 		{return checkFlag(Flag.F11);}
+	public boolean isStone() 		{return checkFlag(Flag.F12);}
+	public boolean isIce() 			{return checkFlag(Flag.F13);}
 	
-	public boolean isDownLadder() {
-		return checkFlag(Flag.F32);
-	}
-	
-	public boolean isPoisioning() {
-		return checkFlag(Flag.F15);
-	}
-	
-	public boolean isFlaming() {
-		return checkFlag(Flag.F16);
-	}
-	
-	public boolean isWet() {
-		return checkFlag(Flag.F17);
-	}
-	
-	public boolean isLantern() {
-		return checkFlag(Flag.F3);
-	}
-	
-	public boolean isVisible() {
-		return checkFlag(Flag.F1);
-	}
+	public boolean isPoisioning() 	{return checkFlag(Flag.F14);}
+	public boolean isFlaming() 		{return checkFlag(Flag.F15);}
+	public boolean isWet() 			{return checkFlag(Flag.F16);}
+	public boolean isInstantKille() {return checkFlag(Flag.F17);}
 
-	public boolean isSeen() {
-		return checkFlag(Flag.F2);
-	}
+	public boolean isUpLadder() 	{return checkFlag(Flag.F30);}
+	public boolean isDownLadder() 	{return checkFlag(Flag.F31);}
 	
-	public boolean isTransparent() {
-		return checkFlag(Flag.F4);
-	}
-
-	public void setSeen(boolean newIsSeen) {
-		setFlag(newIsSeen, Flag.F2);
-	}
-
+	public void setVisible(boolean b)		{setFlag(b, Flag.F0);}
+	public void setSeen(boolean b) 			{setFlag(b, Flag.F1);}
+	
 	public Character removeCharacter() {
 		Character c = charHere;
 		charHere = null;
@@ -173,10 +149,6 @@ public class Tile {
 
 	public void dropItem(Item item) {
 		itemsHere.add(item);
-	}
-
-	public void setVisible(boolean b) {
-
 	}
 	
 }
