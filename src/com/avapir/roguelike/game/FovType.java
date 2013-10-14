fb5a108f68654bba7dc38b3210fd2d77c992fd26
Chugunov Vladimir <inflamepheonix@gmail.com>
2013-10-14 07:39:45 +0400

    Новый алгоритм FOV (не факт, что последний). Вышес visible в поле вместо флага из-за проседания производительности

---
+++ src/com/avapir/roguelike/game/FovType.java
@@ -0,0 +1,12 @@
+package com.avapir.roguelike.game;
+
+/**
+ * Different FOV types. Only Precise Permissive
+ * can do square FOV right now.
+ * @author sdatta
+ *
+ */
+public enum FovType
+{
+	SQUARE, CIRCLE;
+}
---
+++ src/com/avapir/roguelike/game/ILosMap.java
@@ -0,0 +1,44 @@
+package com.avapir.roguelike.game;
+
+/**
+ * An interface board that allows visibility alogithms to
+ * decide which points are in the board, which points are
+ * obstacles to this form of visibility, and visit those points
+ * on the board.
+ * 
+ * @author sdatta
+ * 
+ */
+public interface ILosMap {
+
+	/**
+	 * Is the location (x, y) inside the board ?
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean hasTile(int x, int y);
+
+	/**
+	 * Is the location (x, y) an obstacle
+	 * 
+	 * @param x
+	 * @param y
+	 * @return
+	 */
+	public boolean isObstacle(int x, int y);
+
+	/**
+	 * Location (x,y) is visible
+	 * Visit the location (x,y)
+	 * 
+	 * This can involve saving the points in a collection,
+	 * setting flags on a 2D map etc.
+	 * 
+	 * @param x
+	 * @param y
+	 */
+	public void visit(int x, int y);
+
+}
