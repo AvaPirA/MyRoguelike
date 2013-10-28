c006cfdc71005e97150c23ceabe757cdf7a9a266
Alpen Ditrix <inflamepheonix@gmail.com>
2013-10-28 09:02:53 +0400

    Slime AI bugfix

--- src/com/avapir/roguelike/game/ai/SlimeAI.java
+++ src/com/avapir/roguelike/game/ai/SlimeAI.java
@@ -18,15 +18,15 @@ public class SlimeAI extends EasyAI {
 	public void computeAI(final Mob m, final Game g) {
 		final int x = m.getLoc().x;
 		final int y = m.getLoc().y;
-		final Point p = new Point(r.nextInt(2) - 1, r.nextInt(2) - 1);
-		for (int i = x - 4; i < x + 4; i++) {
-			for (int j = y - 4; j < y + 4; j++) {
-				final Tile t = g.getMap().getTile(i, j);
-				if (t != null && t.getMob() != null && t.getMob() == g.getHero()) {
-					p.move(i < x ? -1 : i > x ? 1 : 0, j < y ? -1 : j > y ? 1 : 0);
-				}
-			}
-		}
+		final Point p = new Point(r.nextInt(3) - 1, r.nextInt(3) - 1);
+//		for (int i = x - 4; i < x + 4; i++) {
+//			for (int j = y - 4; j < y + 4; j++) {
+//				final Tile t = g.getMap().getTile(i, j);
+//				if (t != null && t.getMob() != null && t.getMob() == g.getHero()) {
+//					p.move(i < x ? -1 : i > x ? 1 : 0, j < y ? -1 : j > y ? 1 : 0);
+//				}
+//			}
+//		}
 		final Tile t = g.getMap().getTile(m.getX() + p.x, m.getY() + p.y);
 		if (t != null && t.getMob() != null) {
 			if (t.getMob().getName().equals(m.getName())) {
